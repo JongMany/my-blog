3112b1f181576f2f858c289f8fd5c0dad43b0b6b
JongMany <blackberry1114@naver.com>
2025-09-20 11:41:47 +0900

    docs: 포트폴리오 이미지 수정

--- apps/portfolio/src/components/hero/utils/get-rect.ts
+++ apps/portfolio/src/components/hero/utils/get-rect.ts
@@ -1,11 +1,11 @@
-// export function getRect(container: HTMLElement, element: HTMLElement) {
-//   const containerRect = container.getBoundingClientRect();
-//   const elementRect = element.getBoundingClientRect();
+export function getRect(container: HTMLElement, element: HTMLElement) {
+  const containerRect = container.getBoundingClientRect();
+  const elementRect = element.getBoundingClientRect();
 
-//   return {
-//     left: elementRect.left - containerRect.left,
-//     top: elementRect.top - containerRect.top,
-//     width: elementRect.width,
-//     height: elementRect.height,
-//   };
-// }
+  return {
+    left: elementRect.left - containerRect.left,
+    top: elementRect.top - containerRect.top,
+    width: elementRect.width,
+    height: elementRect.height,
+  };
+}
--- apps/portfolio/src/components/hero/utils/prepare-outgoing.ts
+++ apps/portfolio/src/components/hero/utils/prepare-outgoing.ts
@@ -1,10 +1,10 @@
-// export function prepareOutgoing(element: HTMLElement) {
-//   const rect = element.getBoundingClientRect();
+export function prepareOutgoing(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
 
-//   element.style.position = "fixed";
-//   element.style.top = `${rect.top}px`;
-//   element.style.left = `${rect.left}px`;
-//   element.style.width = `${rect.width}px`;
-//   element.style.height = `${rect.height}px`;
-//   element.style.zIndex = "9999";
-// }
+  element.style.position = "fixed";
+  element.style.top = `${rect.top}px`;
+  element.style.left = `${rect.left}px`;
+  element.style.width = `${rect.width}px`;
+  element.style.height = `${rect.height}px`;
+  element.style.zIndex = "9999";
+}
