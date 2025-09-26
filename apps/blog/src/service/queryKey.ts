4c8576b949d74c3e5020161b22e5ed5bfabe44be
JongMany <blackberry1114@naver.com>
2025-09-26 12:56:10 +0900

    refactor(blog): 필요없는 코드 제거

--- apps/blog/src/service/queryKey.ts
+++
@@ -1,6 +0,0 @@
-export const qk = {
-  posts: () => ["posts"] as const,
-  post: (id: string) => ["post", id] as const,
-  categories: () => ["categories"] as const,
-  postsByCategory: (slug: string) => ["postsByCategory", slug] as const,
-};
--- apps/blog/src/service/types.ts
+++
@@ -1,9 +0,0 @@
-export type Post = {
-  id: string;
-  title: string;
-  content: string;
-  categories: string[]; // slug 배열
-  createdAt: number;
-};
-
-export type Category = { name: string; slug: string };
