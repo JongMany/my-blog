c15ddab7415bcc4ee3a8b6b8f08eb2268700994f
JongMany <blackberry1114@naver.com>
2025-11-21 23:33:51 +0900

    feat(blog): page transition 로직 추가

--- apps/blog/src/components/layout.tsx
+++ apps/blog/src/components/layout.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useLocation, useOutlet } from "react-router-dom";
 import CategoryNavigation from "./category-navigation";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Layout() {
+  const location = useLocation();
+  const outlet = useOutlet();
+
   return (
     <div className="px-2.5">
       {/* 카테고리 네비게이션 */}
       <CategoryNavigation />
 
       {/* 메인 콘텐츠 영역 */}
-      <div className="">
-        <Outlet />
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {outlet}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
