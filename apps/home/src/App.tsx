5e9f6c795fdb9b3363180fbfbb870c47ed410283
JongMany <blackberry1114@naver.com>
2025-11-22 18:45:49 +0900

    fix(home): react-router warn 수정

--- apps/home/src/App.tsx
+++ apps/home/src/App.tsx
@@ -1,11 +1,6 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 
 export default function HomeApp() {
-  return (
-    <Routes>
-      <Route index element={<HomePage />} />
-    </Routes>
-  );
+  return <HomePage />;
 }
