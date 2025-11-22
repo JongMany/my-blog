974c9883335e846e09dbc2cdc0af2645020aa5fb
JongMany <blackberry1114@naver.com>
2025-11-22 17:47:49 +0900

    fix(home): 빌드 에러 수정

--- apps/home/src/components/index.ts
+++ apps/home/src/components/index.ts
@@ -1,5 +1,5 @@
 export { HomeHeader } from "./HomeHeader";
 export { SectionCard } from "./SectionCard";
 export { SectionGrid } from "./SectionGrid";
-export type { Section } from "../types/section";
+export type { Section } from "../consts/sections";
 
