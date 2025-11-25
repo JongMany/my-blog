b22e632ab13983b2a2791c3dc3246113502e91b6
JongMany <blackberry1114@naver.com>
2025-11-25 20:58:59 +0900

    feat(home): home layout 수정

--- apps/home/src/pages/HomePage.tsx
+++ apps/home/src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import { sections } from "../consts/sections";
 
 export default function HomePage() {
   return (
-    <div className={cn("min-h-screen bg-white", "p-6 sm:p-8 md:p-12")}>
+    <div className={cn("min-h-screen bg-white", "px-6 sm:px-8 md:px-12 py-2")}>
       <div className="max-w-7xl mx-auto">
         <HomeHeader />
         <SectionGrid sections={sections} />
