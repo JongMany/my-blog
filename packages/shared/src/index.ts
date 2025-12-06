38d00f24eb5e9a3445462f5da9d606eb28072efb
JongMany <blackberry1114@naver.com>
2025-12-07 01:20:26 +0900

    refactor(shared): 리액트 비의존 유틸을 utils 패키지로 마이그레이션
    - type-guards, frontmatter, date, jsonp 파일 삭제
    - analytics.ts에서 @srf/utils의 requestJsonp 사용
    - index.ts에서 하위 호환성을 위해 utils 함수들 re-export
    - package.json에 @srf/utils 의존성 추가
    
    이제 shared는 React에 의존하는 코드만 포함합니다.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,28 @@ export * from "./query/keys";
 export * from "./utils";
 export * from "./hooks";
 export * from "./context";
+
+// 하위 호환성을 위해 utils에서 re-export (deprecated: @srf/utils를 직접 사용하세요)
+export {
+  isString,
+  isNumber,
+  isBoolean,
+  isArray,
+  isPlainObject as isObject,
+} from "@srf/utils";
+export {
+  parseFrontmatter,
+  getString,
+  getOptionalString,
+} from "@srf/utils";
+export {
+  extractDateFromMeta,
+  formatDate,
+  compareByDate,
+  filterAndSortByDate,
+  filterPublishedItems,
+  type DateMeta,
+  type DateComparable,
+  type SortableByDate,
+} from "@srf/utils";
+export { requestJsonp } from "@srf/utils";
--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,6 +1,2 @@
 export * from "./image-source";
-export * from "./type-guards";
-export * from "./jsonp";
 export * from "./analytics";
-export * from "./frontmatter";
-export * from "./date";
